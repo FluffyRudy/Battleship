4755c1f46190694a59f617fe951c4f53d4510cdf
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-02-16 18:58:47 +0545

    Initial setup

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Fluffy Restaurent',
+            favicon: `${__dirname}/src/assets/favicon.ico`,
+            template: './src/template.html',
+        })
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ico|png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(css)$/i,
+                use: ['style-loader', 'css-loader', "postcss-loader"],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
