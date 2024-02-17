ba437c9d73bf9f3fdda22e041ad4e2c9317aa6d7
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-02-17 12:56:47 +0545

    Add colors for human and ai ship

--- src/script/lib/settings.js
+++ src/script/lib/settings.js
@@ -2,7 +2,9 @@ const settings = {
     NROWS: 10,
     NCOLS: 10,
     totalShip: 5,
-    shipLengths: [5, 4, 3, 3, 2]
+    shipLengths: [5, 4, 3, 3, 2],
+    aiShipColor: "#000",
+    humanShipColor: "#fff"
 }
 
 export default settings;
